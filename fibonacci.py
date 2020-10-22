90b13352a7a3ec1ee34c18c70f75e6d5a718c54b
GiovaneThomazin <69993080+GiovaneThomazin@users.noreply.github.com>
2020-10-22 20:19:43 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,24 +1,16 @@
- importar os
+import os
 from flask import Flask, jsonify, request
-de matemática import sqrt
+from math import sqrt
 
+app = Flask(__name__)
 
-app = Flask (__name__)
+@app.route('/')
+def nao_entre_em_panico():
 
-@ app . route('/')
-def nao_entre_em_panico ():
-    proximo = 1
-    anterior - 0
-    limite = 98
-    encontrado = 0
-    resposta = "1, \ n"
-    while (encontrado < limite );
-    	tmp = proximo
-	proximo = proximo + anterior
-	anterior = tmp
-	encontrado = encontrado + 1
-	resposta + = str (proximo ) + ", \ n "
-    reposta de retorno
-if __name__ = "__main__" :
-	porta = int (os .amb . get ( "PORT" , 5000))
-	app . executar (host = '0.0.0.0" , porta = porta )
+	primos = "Docker esta funcionando"
+
+  return primos
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
